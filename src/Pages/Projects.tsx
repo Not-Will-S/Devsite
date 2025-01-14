0b4ab914ec5319e1adc41097acf20a10dee8584b
Not-Will-S <110842579+Not-Will-S@users.noreply.github.com>
2025-01-14 01:19:33 +0000

    added another project and small fixes

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -2,6 +2,7 @@ import ProjectInstance from "../Components/ProjectInstance.tsx";
 import Ant from "../Images/ant_graphic.png"
 import ReactSymbol from "../Images/react.png"
 import PlayingCards from "../Images/playing-cards.png"
+import CreditCard from "../Images/Credit-Card.png"
 
 function Projects(){
 
@@ -38,10 +39,20 @@ function Projects(){
                 
                 <ProjectInstance 
                     projectTitle="Multi-Threaded Card player"
-                    projectDescription="This project was created for a second year University Module and shows a competancy in Java and an understanding of Multi-Threading. In this project, any number of players may draw cards from a shared deck and will continue to exchange cards with other players until the win condition for a player has been reached. Each player operates on its own thread simultaneously and each card is reppresented by an object that can be locked and/or unlocked by threads depending on which player has ownership."
-                    githubLink="Sample link"
+                    projectDescription="This project was created for a second year University Module and shows a competancy in Java and an understanding of Multi-Threading. In this project, any number of players may draw cards from a shared deck and will continue to exchange cards with other players until the win condition for a player has been reached. Each player operates on its own thread simultaneously and each card is represented by an object that can be locked and/or unlocked by threads depending on which player has ownership."
+                    githubLink="https://github.com/Not-Will-S"
                     skillsUsed={["Java", "Multi-Threading", "Pair Programming"]}
                     imageLink={PlayingCards}
+                />
+
+                <ProjectInstance 
+                    projectTitle="Machine Learning Credit-Card default prediction"
+                    projectDescription="This project was created for a second year University Module. In this project a machine learning model was created with the aim of classifiying credit hard holder data as likely to default and unlikely to default. 
+                    Two machine learning models were combined to make an ensemble model, a multi-layer-perceptron was used for data where there was no chronological attributes and a Reccurent neural network was used for data points where there were temporal elements.
+                    Output for these models were then fed into an additional Multi-Layer-Perceptron in charge of overall classification. "
+                    githubLink="https://github.com/Not-Will-S/Credit-Card-Prediction"
+                    skillsUsed={["Python", "Machine-Learning", "Data Preprocesing"]}
+                    imageLink={CreditCard}
                 />
             </div>
         </div>
